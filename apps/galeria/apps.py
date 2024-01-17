1c099b81c4faba8c1ea33d52ec7100f654299c5f
Raimundo Cunha <rrmcunha@example.com>
2024-01-17 13:37:38 -0300

    Refatorando pastas e ccs

--- apps/galeria/apps.py
+++ apps/galeria/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class GaleriaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'galeria'
+    name = 'apps.galeria'
--- apps/usuarios/forms.py
+++ apps/usuarios/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class LoginForms(forms.Form):
     nome_login=forms.CharField(
-        label='Nome de Login', 
+        label='Nome de Usuário', 
         required=True, 
         max_length=100,
         widget=forms.TextInput(
